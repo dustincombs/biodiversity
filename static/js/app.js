09cf33190178951bc22ff04517acae881479f221
dustincombs <dccombs@gmail.com>
2020-12-15 16:19:02 -0500

    initial commit

---
+++ static/js/app.js
@@ -0,0 +1,103 @@
+var bbdata = JSON.parse(data)
+var metadata = bbdata.metadata
+var samples = bbdata.samples
+
+// select items from the html
+var demoPanel = d3.select("#sample-metadata")
+var select = d3.select("#selDataset")
+
+// populate drop down list with IDs from the data
+bbdata.names.forEach((item, i) => {
+  var option = select.append("option")
+  option.text(item)
+  option.property("value",item)
+});
+
+// fill the demographic information
+function fillDemoPanel(info){
+  // clear the panel
+  demoPanel.html("")
+  // create an unordered list
+  infoList = demoPanel.append("ul")
+  infoList.attr("class","list-group")
+  // iterate over key value pairs and append a list item
+  // for each pair
+  Object.entries(info).forEach(([key, value]) => {
+    var listItem = infoList.append("li")
+    listItem.text(`${key} : ${value}`)
+    listItem.attr("class","list-group-item")
+    listItem.attr("style","border: none")
+  })
+}
+
+function drawGaugePlot(wfreq){
+  var trace = {
+    value: wfreq,
+    title: {text: "Washing Frequency (per Week)"},
+    type: "indicator",
+    mode: "gauge+number",
+    gauge: {
+      axis: {
+        range: [null,10],
+        tickvals: [0,2,4,6,8,10]},
+      bar: {color: '#1f77b4'}
+    }
+  }
+  Plotly.newPlot("gauge",[trace])
+}
+
+function drawBarPlot(sample){
+  var samples = sample.sample_values.slice(0,10)
+  samples.reverse()
+  var otu_ids = sample.otu_ids.slice(0,10)
+  otu_ids.reverse()
+  var otu_labels = sample.otu_labels.slice(0,10)
+  otu_labels.reverse()
+  var trace = {
+    x: samples,
+    y: otu_ids.map(x => `OTU ${x}`),
+    text: otu_labels,
+    type: "bar",
+    orientation: "h"
+  }
+  var layout = {
+    xaxis: {
+      title: "Sample Values"
+    }
+  }
+  Plotly.newPlot("bar",[trace],layout)
+}
+
+function drawBubblePlot(sample){
+  var trace = {
+    x: sample.otu_ids,
+    y: sample.sample_values,
+    text: sample.otu_labels,
+    mode: "markers",
+    marker: {
+      size: sample.sample_values,
+      color: sample.otu_ids,
+      colorscale: "Viridis"
+    },
+    type: "scatter",
+  }
+  var layout = {
+    xaxis: {
+      title: "OTU ID"
+    },
+    yaxis: {
+      title: "Sample Values"
+    }
+  }
+  Plotly.newPlot("bubble",[trace],layout)
+}
+
+function optionChanged(value){
+  var demoData = metadata.find(x => x.id == value)
+  fillDemoPanel(demoData)
+  drawGaugePlot(demoData.wfreq)
+  var sample = samples.find(x => x.id == value)
+  drawBarPlot(sample)
+  drawBubblePlot(sample)
+  console.log(sample)
+}
